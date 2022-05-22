5c0676ef607738abf13cd74857efa00d49f23616
sensorbtf <100487972+sensorbtf@users.noreply.github.com>
2022-05-22 11:36:20 +0200

    Final Version
    Dodanie: algorytmu dla PC (działa po klikaniu), przycisku resetowania gry po jej zakończeniu i monitu co do jej zakończenia.

--- WPF 03 - Layout + Attached Property/MainWindow.xaml.cs
+++ WPF 03 - Layout + Attached Property/MainWindow.xaml.cs
@@ -20,14 +20,25 @@ namespace WPF_03___Layout___Attached_Property
     /// </summary>
     public partial class MainWindow : Window
     {
-        private int _round = 1;
+        private int _round = 0;
+        int randomForButton;
+        int PCMovement = 0;
+        Random random = new Random();
+
         public MainWindow()
         {
             InitializeComponent();
         }
-        private void Button_Click(object sender, RoutedEventArgs click)
+        private void Button_Click_Reset(object sender, RoutedEventArgs click)
         {
-            if (_round %)
+            MainWindow newWindow = new MainWindow();
+            Application.Current.MainWindow = newWindow;
+            newWindow.Show();
+            this.Close();
+        }
+            private void Button_Click(object sender, RoutedEventArgs click)
+        {
+            if (_round%2==0)
             {
                 if (sender is Button)
                 {
@@ -36,19 +47,122 @@ namespace WPF_03___Layout___Attached_Property
                     (sender as Button).Content = "X";
 
                     _round++;
+                    PCMovement = 0;
                 }
             }
             else
             {
-                (sender as Button).Content = "O";
-
-                (sender as Button).IsEnabled = false;
-
-                _round++;
+                for (int i = 0; i < 10; i++)
+                {
+                    if (PCMovement == 0)
+                    {
+                        randomForButton = random.Next(1, 9);
+                        CrazyPCWithVeryEfficientScript(randomForButton);
+                    }
+                }
             }
-            if (_round == 10)
-            {
-                Center.Visibility = Visibility.Visible;
+                if (_round == 9)
+                {
+                    EndGameLabel.Visibility = Visibility.Visible;
+                    GameReset.Visibility = Visibility.Visible;
+                }
+        }
+        private void CrazyPCWithVeryEfficientScript(int randomized)
+        {
+
+            switch (randomized)
+                {
+                    case 1:
+                        if (LewyGorny.IsEnabled == true)
+                        {
+                            LewyGorny.Content = "O";
+                            LewyGorny.IsEnabled = false;
+                            _round++;
+                        PCMovement = 1;
+                        break;
+                    }
+                    break;
+                case 2:
+                        if (PrawyGorny.IsEnabled == true)
+                        {
+                            PrawyGorny.Content = "O";
+                            PrawyGorny.IsEnabled = false;
+                            _round++;
+                        PCMovement = 1; 
+                        break;
+                    }
+                    break;
+                case 3:
+                        if (SrodkowyDolny.IsEnabled == true)
+                        {
+                            SrodkowyDolny.Content = "O";
+                            SrodkowyDolny.IsEnabled = false;
+                            _round++;
+                        PCMovement = 1; break;
+
+                    }
+                    break;
+                case 4:
+                        if (SrodkowyGorny.IsEnabled == true)
+                        {
+                            SrodkowyGorny.Content = "O";
+                            SrodkowyGorny.IsEnabled = false;
+                            _round++; 
+                        PCMovement = 1; 
+                        break;
+
+                    }
+                    break;
+
+                case 5:
+                        if (LewyDolny.IsEnabled == true)
+                        {
+                            LewyDolny.Content = "O";
+                            LewyDolny.IsEnabled = false;
+                            _round++;
+                        PCMovement = 1;
+                        break;
+                    }
+                    break;
+
+                case 6:
+                        if (PrawyDolny.IsEnabled == true)
+                        {
+                            PrawyDolny.Content = "O";
+                            PrawyDolny.IsEnabled = false;
+                            _round++; PCMovement = 1; break;
+
+                    }
+                    break;
+                case 7:
+                        if (SrodkowySrodkowy.IsEnabled == true)
+                        {
+                            SrodkowySrodkowy.Content = "O";
+                            SrodkowySrodkowy.IsEnabled = false;
+                            _round++; PCMovement = 1;  break;
+
+                    }
+                    break;
+
+                case 8:
+                        if (SrodkowyDolny.IsEnabled == true)
+                        {
+                            SrodkowyDolny.Content = "O";
+                            SrodkowyDolny.IsEnabled = false;
+                            _round++; PCMovement = 1; break;
+
+                    }
+                    break;
+                case 9:
+                        if (LewySrodkowy.IsEnabled == true)
+                        {
+                            LewySrodkowy.Content = "O";
+                            LewySrodkowy.IsEnabled = false;
+                            _round++; PCMovement = 1; break;
+
+                    }
+                    break;
+
             }
         }
 
